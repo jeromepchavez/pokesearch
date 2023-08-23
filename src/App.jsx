c3a060b2311ed434097fcc8f7bacd107090c178b
Jerome Chavez <chavejer@oregonstate.edu>
2023-08-22 17:20:32 -0700

    imported react and created state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import React, { useState, useEffect } from 'react';
 import { Input } from '@chakra-ui/react';
 
 function App() {
+  const [pokemonNames, setPokemonNames] = useState([]);
 
+  
   return (
     <div>
       <h1>PokeSearch!</h1>
