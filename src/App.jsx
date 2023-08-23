daf0a67a21a01798e4b5d8d4d726dc302ec45179
Jerome Chavez <chavejer@oregonstate.edu>
2023-08-22 17:35:15 -0700

    Created useEffect call to PokeAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { Input } from '@chakra-ui/react';
+import { Input, Box } from '@chakra-ui/react';
+import axios from 'axios';
 
 function App() {
   const [pokemonNames, setPokemonNames] = useState([]);
 
-  
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit-1100')
+      .then(data => console.log(data.data))
+      .catch(error => console.log(error))
+  })
+
   return (
-    <div>
+    <Box>
       <h1>PokeSearch!</h1>
       <Input />
-    </div>
+    </Box>
   )
 }
 
