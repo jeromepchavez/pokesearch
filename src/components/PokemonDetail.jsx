3dd19e2d969e881f7212514dad86b2d976bc1026
Jerome Chavez <chavejer@oregonstate.edu>
2023-08-26 21:44:24 -0700

    added images for pokemon types

--- src/components/PokemonDetail.jsx
+++ src/components/PokemonDetail.jsx
@@ -7,47 +7,65 @@ import {
           Card,
           CardHeader,
           CardBody,
-          CardFooter,
           HStack,
           Image,
           Badge
         } from '@chakra-ui/react';
+
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 
 const PokemonDetail = ({ pokemon }) => {
   if (!pokemon) {
     return <Center margin="20px">No Pokémon selected.</Center>;
   }
 
-  //Height is given in decimeters. Function to convert to feet/inches.
-  const calculateHeight = () => {
-
+  //Height is given in decimeters. Function to convert to feet/inches. divide height by 3.048
+  const calculateFeet = (height) => {
+    return Math.floor(height / 3.048)
   };
+
+  const calculateInches = (height) => {
+    return (height % 3.048)
+  }
 
   return (
     <Box>
       <Center>
         <Card boxShadow='dark-lg' maxWidth='1000px'>
-          <CardHeader>
-            <Center>
-              <Heading>{pokemon.name.toUpperCase()}</Heading>
-            </Center>
-          </CardHeader>
-          <CardBody>
-            <HStack spacing='400px' justifyContent='center'>
-              <Badge>ORIGINAL</Badge>
-              <Badge colorScheme='purple'>SHINY</Badge>
-            </HStack>
-            <HStack margin ='10px' spacing='30px' justifyContent='center'>
-              <Image boxSize='400px' src={pokemon.imageUrl} alt={pokemon.name} />
-              <Image boxSize='400px' src={pokemon.shinyUrl} alt={pokemon.name} />
-            </HStack>
-            <div>Type: {pokemon.type}</div>
-            <div>Height: {pokemon.height}</div>
-            <div>Weight: {pokemon.weight}</div>
-            <div>ID: {pokemon.pokemonID}</div>
-            <div>Pokedex: {pokemon.pokedexEntry}</div>
-            <div>Generation: {pokemon.generation}</div>
-          </CardBody>
+          <Tabs variant='enclosed'>
+            <TabList>
+              <Tab>INFO</Tab>
+              <Tab>Two</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <CardHeader>
+                  <Center>
+                    <Heading>{pokemon.name.toUpperCase()}</Heading>
+                  </Center>
+                </CardHeader>
+                <CardBody>
+                  <HStack spacing='400px' justifyContent='center'>
+                    <Badge>ORIGINAL</Badge>
+                    <Badge colorScheme='purple'>SHINY</Badge>
+                  </HStack>
+                  <HStack margin ='10px' spacing='30px' justifyContent='center'>
+                    <Image boxSize='400px' src={pokemon.imageUrl} alt={pokemon.name} />
+                    <Image boxSize='400px' src={pokemon.shinyUrl} alt={pokemon.name} />
+                  </HStack>
+                  <div>Type: {pokemon.type}</div>
+                  <div>Height: {calculateFeet(pokemon.height)}' {calculateInches(pokemon.height)}"</div>
+                  <div>Weight: {pokemon.weight}</div>
+                  <div>ID: {pokemon.pokemonID}</div>
+                  <div>Pokedex: {pokemon.pokedexEntry}</div>
+                  <div>Generation: {pokemon.generation}</div>
+                </CardBody>
+              </TabPanel>
+              <TabPanel>
+                <p>two!</p>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </Card>
       </Center>
     </Box>
