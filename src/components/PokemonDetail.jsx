ef156d41705f78f58963735f05c59495b4f69468
Jerome Chavez <chavejer@oregonstate.edu>
2023-08-28 18:30:30 -0700

    Removed Tabs components

--- src/components/PokemonDetail.jsx
+++ src/components/PokemonDetail.jsx
@@ -12,58 +12,48 @@ import {
           Badge
         } from '@chakra-ui/react';
 
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-
 const PokemonDetail = ({ pokemon }) => {
   if (!pokemon) {
     return <Center margin="20px">No Pokémon selected.</Center>;
   }
 
   //Height is given in decimeters. Function to convert to feet/inches. divide height by 3.048
-  const calculateFeet = (height) => {
-    return Math.floor(height / 3.048)
+  const calculateFeetInches = (height) => {
+    let feet = Math.floor(height / 3.048)
+    let inches = parseFloat((height % 3.048).toFixed(2))
+    return feet+ `'` + inches + `"`
   };
-
-  const calculateInches = (height) => {
-    return (height % 3.048)
-  }
 
   return (
     <Box>
       <Center>
         <Card boxShadow='dark-lg' maxWidth='1000px'>
-          <Tabs isFitted variant='enclosed-colored' size='lg'>
-            <TabList>
-              <Tab>INFO</Tab>
-            </TabList>
-            <TabPanels>
-              <TabPanel>
-                <CardHeader>
-                  <Center>
-                    <Heading>{pokemon.name.toUpperCase()}</Heading>
-                  </Center>
-                </CardHeader>
-                <CardBody>
-                  <HStack spacing='400px' justifyContent='center'>
-                    <Badge>ORIGINAL</Badge>
-                    <Badge colorScheme='purple'>SHINY</Badge>
-                  </HStack>
-                  <HStack margin ='10px' spacing='30px' justifyContent='center'>
-                    <Image boxSize='400px' src={pokemon.imageUrl} alt={pokemon.name} />
-                    <Image boxSize='400px' src={pokemon.shinyUrl} alt={pokemon.name} />
-                  </HStack>
-                  <div>Type: {pokemon.type.map((type, index) => (
-                    <div key={index}>{type.type.name.toUpperCase()}</div>
-                    ))}</div>
-                  <div>Height: {calculateFeet(pokemon.height)}' {calculateInches(pokemon.height)}"</div>
-                  <div>Weight: {pokemon.weight}</div>
-                  <div>ID: {pokemon.pokemonID}</div>
-                  <div>Pokedex: {pokemon.pokedexEntry}</div>
-                  <div>Generation: {pokemon.generation}</div>
-                </CardBody>
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
+          <CardHeader>
+            <Center>
+              <Heading>{pokemon.name.toUpperCase()}</Heading>
+            </Center>
+          </CardHeader>
+          <CardBody>
+            <HStack spacing='400px' justifyContent='center'>
+              <Badge>ORIGINAL</Badge>
+              <Badge colorScheme='purple'>SHINY</Badge>
+            </HStack>
+            <HStack margin ='10px' spacing='30px' justifyContent='center'>
+              <Image boxSize='400px' src={pokemon.imageUrl} alt={pokemon.name} />
+              <Image boxSize='400px' src={pokemon.shinyUrl} alt={pokemon.name} />
+            </HStack>
+            <div>Type: {pokemon.type.map((type, index) => (
+              <div key={index}>{type.type.name.toUpperCase()}</div>
+              ))}</div>
+            <div>Height: {calculateFeetInches(pokemon.height)}</div>
+            <div>Weight: {
+              //Weight from the GET call is in hectograms. Converting to pounds
+              (pokemon.weight/4.536).toFixed(1)
+              } lbs.</div>
+            <div>ID: {pokemon.pokemonID}</div>
+            <div>Pokedex: {pokemon.pokedexEntry}</div>
+            <div>Generation: {pokemon.generation}</div>
+          </CardBody>
         </Card>
       </Center>
     </Box>
