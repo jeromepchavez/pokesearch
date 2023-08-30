69d69d3ea48307b34b08797f5231ec168203daa5
Jerome Chavez <chavejer@oregonstate.edu>
2023-08-29 20:53:34 -0700

    Imported cleaner svg for pokemon type

--- src/components/PokemonDetail.jsx
+++ src/components/PokemonDetail.jsx
@@ -11,6 +11,7 @@ import {
           Image,
           Badge
         } from '@chakra-ui/react';
+import '../css/pokemontype.css'
 
 const PokemonDetail = ({ pokemon }) => {
   if (!pokemon) {
@@ -23,6 +24,48 @@ const PokemonDetail = ({ pokemon }) => {
     let inches = parseFloat((height % 3.048).toFixed(2))
     return feet+ `'` + inches + `"`
   };
+
+  //Function that also returns the corresponding image of the Pokemon type
+  const renderPokemonType = (type) => {
+    switch(type) {
+      case type = 'fire':
+        return "src/assets/pokemon-types/fire.svg"
+      case type = 'flying':
+        return "src/assets/pokemon-types/flying.svg"
+      case type = 'electric':
+        return "src/assets/pokemon-types/electric.svg"
+      case type = 'ghost':
+          return "src/assets/pokemon-types/ghost.svg"
+      case type = 'fighting':
+        return "src/assets/pokemon-types/fighting.svg"
+      case type = 'water':
+        return "src/assets/pokemon-types/water.svg"
+      case type = 'grass':
+        return "src/assets/pokemon-types/grass.svg"
+      case type = 'fairy':
+        return "src/assets/pokemon-types/fairy.svg"
+      case type = 'dark':
+        return "src/assets/pokemon-types/dark.svg"
+      case type = 'dragon':
+        return "src/assets/pokemon-types/dragon.svg"
+      case type = 'poison':
+        return "src/assets/pokemon-types/poison.svg"
+      case type = 'psychic':
+        return "src/assets/pokemon-types/psychic.svg"
+      case type = 'rock':
+        return "src/assets/pokemon-types/rock.svg"
+      case type = 'steel':
+        return "src/assets/pokemon-types/steel.svg"
+      case type = 'ice':
+        return "src/assets/pokemon-types/ice.svg"
+      case type = 'ground':
+        return "src/assets/pokemon-types/ground.svg"
+      case type = 'normal':
+        return "src/assets/pokemon-types/normal.svg"
+      case type = 'bug':
+        return "src/assets/pokemon-types/bug.svg"
+    }
+  }
 
   return (
     <Box>
@@ -42,8 +85,17 @@ const PokemonDetail = ({ pokemon }) => {
               <Image boxSize='400px' src={pokemon.imageUrl} alt={pokemon.name} />
               <Image boxSize='400px' src={pokemon.shinyUrl} alt={pokemon.name} />
             </HStack>
-            <div>Type: {pokemon.type.map((type, index) => (
-              <div key={index}>{type.type.name.toUpperCase()}</div>
+            <div>Type: {pokemon.type.map((type) => (
+              <div>
+                <div key={type.slot}>
+                  {type.type.name.toUpperCase()}
+                  <img 
+                    className={type.type.name + " icon"} 
+                    src={renderPokemonType(type.type.name)} 
+                    alt={type.type.name} 
+                  />
+                </div>
+              </div> 
               ))}</div>
             <div>Height: {calculateFeetInches(pokemon.height)}</div>
             <div>Weight: {
